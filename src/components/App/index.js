df41d8f6ba68c5344e7b5578e1ac02cf77f0bc40
mirenguerra <miren.guerra.sanz@gmail.com>
2019-07-23 16:53:17 +0200

    Create services and data json

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,39 @@
 import React from "react";
 import "./styles.scss";
 import InputFilter from "../InputFilter";
+import getDataList from '../../services/getAdalabersDataList';
+import getAdalaberData from "../../services/getAdalaberData";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      adalabersList: [],
+      selectedAdalaber: "",
+    };
   }
+
+  componentDidMount() {
+    if (this.state.adalabersList.length === 0) {
+      this.fetchAdalabersList();
+    }
+  }
+
+  fetchAdalabersList() {
+    getDataList().then(users => {
+      users.map(user => {                
+        return this.fetchAdalaberData(user.url);
+      });
+    });
+  }
+
+  fetchAdalaberData(url){
+    console.log(url);
+    getAdalaberData(url).then(userData=>{
+      console.log(userData);
+    })
+  }
+
   render() {
     return (
       <div className="App">
