8691514c0512a20ef58252d5d4beac83c243c189
mirenguerra <miren.guerra.sanz@gmail.com>
2019-07-23 17:08:49 +0200

    Fetch to get adalabersList and adalaberData

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./styles.scss";
 import InputFilter from "../InputFilter";
-import getDataList from '../../services/getAdalabersDataList';
+import getDataList from "../../services/getAdalabersDataList";
 import getAdalaberData from "../../services/getAdalaberData";
+const adalabersData = [];
 
 class App extends React.Component {
   constructor(props) {
@@ -21,17 +22,25 @@ class App extends React.Component {
 
   fetchAdalabersList() {
     getDataList().then(users => {
-      users.map(user => {                
+      users.map(user => {
         return this.fetchAdalaberData(user.url);
       });
     });
   }
 
-  fetchAdalaberData(url){
-    console.log(url);
-    getAdalaberData(url).then(userData=>{
-      console.log(userData);
-    })
+  fetchAdalaberData(url) {
+    getAdalaberData(url).then(data => {
+      adalabersData
+        .push(data)
+        .sort((a, b) =>
+          a.login.toLowerCase() > b.login.toLowerCase()
+            ? 1
+            : b.login.toLowerCase() > a.login.toLowerCase()
+            ? -1
+            : 0
+        );
+      this.setState({ adalabersList: [...adalabersData] });
+    });
   }
 
   render() {
--- src/services/getAdalabersDataList.js
+++ src/services/getAdalabersDataList.js
@@ -1,4 +1,5 @@
-const URL = "https://api.github.com/orgs/adalab/members?per_page=68";
+// const URL = "https://api.github.com/orgs/adalab/members?per_page=68";
+const URL = 'https://raw.githubusercontent.com/Adalab/f-online-github-card-mirenguerra/dev/src/data/data.json';
 
 const getDataList = () => {
   return fetch(URL).then(res => res.json());
