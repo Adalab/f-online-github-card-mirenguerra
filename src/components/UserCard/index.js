dbc8df124504d4ef3a3cb04116f8948d23b557d6
mirenguerra <miren.guerra.sanz@gmail.com>
2019-07-23 17:58:50 +0200

    Print card

--- src/components/UserCard/index.js
+++ src/components/UserCard/index.js
@@ -1,13 +1,63 @@
-import React from 'react';
-import './styles.scss';
+import React from "react";
+import "./styles.scss";
 
-const UserCard = props =>{
-    const {adalabersList, selectedAdalaber}=props;
-    return(
+const UserCard = props => {
+  const { adalabersList, selectedAdalaber } = props;
+  if (selectedAdalaber) {
+    const adalaber = adalabersList.filter(
+      adalaber => adalaber.login === selectedAdalaber
+    );
+    let currentAdalaber = adalaber[0];
+
+    return (
       <section className="UserCard">
-          
+        <div
+          className="UserCard__img"
+          src={currentAdalaber.avatar_url}
+          alt={`Foto de ${currentAdalaber.login}`}
+        />
+        <div className="UserCard__info">
+          <a
+            href={currentAdalaber.html_url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="UserCard__username"
+          >
+            @{currentAdalaber.login}
+          </a>
+          <h1 className="UserCard__name">{currentAdalaber.name}</h1>
+          {currentAdalaber.location ? (
+            <h2 className="UserCard__location">
+              <i className="fas fa-map-marker-alt" />
+              {currentAdalaber.location}
+            </h2>
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="UserCard__GitHub">
+          <div className="UserCard__GitHub-info">
+            <span className="UserCard__GitHub-info-number">
+              {currentAdalaber.public_repos}
+            </span>
+            <h2 className="UserCard__GitHub-info-text">Repos</h2>
+          </div>
+          <div className="UserCard__GitHub-info">
+            <span className="UserCard__GitHub-info-number">
+              {currentAdalaber.followers}
+            </span>
+            <h2 className="UserCard__GitHub-info-text">Followers</h2>
+          </div>
+          <div className="UserCard__GitHub-info">
+            <span className="UserCard__GitHub-info-number">
+              {currentAdalaber.following}
+            </span>
+            <h2 className="UserCard__GitHub-info-text">Following</h2>
+          </div>
+        </div>
       </section>
-    )
-}
+    );
+  }
+};
 
 export default UserCard;
