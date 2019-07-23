ae42268d0317f851b29c2283557c750a158d73be
mirenguerra <miren.guerra.sanz@gmail.com>
2019-07-23 14:48:48 +0200

    Initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.sass";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={}
+  }
+  render() {
+    return <div className="App" />;
+  }
+}
+
+export default App;
